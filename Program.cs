aa7bcbdc6561d8a649bc3903094188e737732f6b
black <black@example.com>
2026-07-02 23:53:52 +0500

    Change greeting
    :wq
    Enterre currently rebasing branch 'main' on '2cfb6b9'.
    :wq
    Entersc
    :wq
    Enter

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello");
 
 void greetWhite()
 {
     Console.WriteLine("hello white");
 }
 
-Console.WriteLine("hello");
+Console.WriteLine("hello everyone");
